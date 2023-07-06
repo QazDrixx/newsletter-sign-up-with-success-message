8e86473f23b41a64bb812bbb5af28bb3ea4f3d3f
QazDrixx <pugach0548@gmail.com>
2023-07-06 13:14:30 +0300

    modified:   index.html 	modified:   src/css/style.css 	modified:   src/js/script.js

--- src/js/script.js
+++ src/js/script.js
@@ -1,21 +1,35 @@
 document.addEventListener('DOMContentLoaded', () => {
-    let btns = document.querySelectorAll('.button'),
-        wrap = document.querySelector('.wrap'),
+    let wrap = document.querySelector('.wrap'),
         success_wrap = document.querySelector('.success-wrap'),
-        form_input = document.querySelector('.form__input')
+        form_input = document.querySelector('.form__input'),
+        success_message = document.querySelector('.success-message'),
+        dismiss_button = document.querySelector('.dismiss__button'),
+        form = document.querySelector('.form'),
         isEmail = false;
 
-    btns.forEach(el => el.addEventListener('click', (e) => {
-        e.preventDefault()
+    function changeMessage () {
         if (isEmail) {
             wrap.classList.toggle('hide')
             success_wrap.classList.toggle('hide')
+            success_message.innerHTML = `A confirmation email has been sent to <b>${form_input.value}</b> Please open it and click the button inside to confirm your subscription.`
             form_input.value = ''
             form_input.classList.remove('is-valid')
+            isEmail = false;
         }
-    }))
+    }
+
+    form.addEventListener('submit', (e) => {
+        e.preventDefault()
+        changeMessage()
+    })
+
+    dismiss_button.addEventListener('click', () => {
+        isEmail = true;
+        changeMessage()
+    })
 
     form_input.addEventListener('input', (e) => {
+        isEmail = false;
         if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(form_input.value)) {
             form_input.classList.add('is-invalid')
             isEmail = false
@@ -27,7 +41,3 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     })
 })
-
-let a = ''
-
-a.includes
